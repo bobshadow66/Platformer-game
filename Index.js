0c9e2aea01f402fc20e1d629722a882a351ff3da
bobshadow66 <eebooby@gmail.com>
2025-02-22 18:19:55 -0700

    - Stopped continous movement of red block when direction was pressed

--- Index.js
+++ Index.js
@@ -5,50 +5,79 @@ canvas.width = 1024
 canvas.height = 576
 
 const gravity = 0.5
-class Player {
-    constructor(position) {
-      this.position = position
-      this.velocity = {
-        x: 0,
-        y: 1,
-      }
-      this.height = 100
-    }
 
-    draw() {
+class Sprite {
+  constructor(position, imageSrc){
+    this.position = position
+    this.image = new Image()
+    this.image.src = imageSrc
+    this.image.onload = () => {
+      this.loaded = true
+    }
+  }
+  draw() {
+    if (!this.loaded) return
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+
+  update() {
+    this.draw()
+  }
+}
+
+class Player {
+  constructor(position) {
+    this.position = position
+    this.velocity = {
+      x: 0,
+      y: 1,
+    }
+    this.height = 100
+  }
+
+  draw() {
     c.fillStyle = 'red'
     c.fillRect(this.position.x, this.position.y, 100, this.height) 
-   }
+  }
 
-   update() {
+  update() {
     this.draw()
-    
     this.position.x += this.velocity.x
     this.position.y += this.velocity.y
 
-    if (this.position.y + this.height + this.velocity.y < canvas.height)
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
       this.velocity.y += gravity
-    else this.velocity.y = 0
-   }
+    } else {
+      this.velocity.y = 0
+    }
+  }
 }
 
-const player = new Player({
-    x: 0,
-    y: 0,
-})
-const player2 = new Player({
-    x: 300,
-    y: 100, 
+const player = new Player({ x: 0, y: 0 })
+const player2 = new Player({ x: 300, y: 100 })
+
+const keys = {
+  d: { pressed: false },
+  a: { pressed: false },
+}
+
+const background = new Sprite({
+  position: { x: 0, y: 0 },
+  imageSrc: './Assests/background.png',
 })
 
-let y = 100
-let y2 = 100
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'white'
   c.fillRect(0, 0, canvas.width, canvas.height)
+
+  background.update()
   player.update()
   player2.update()
+
+  player.velocity.x = 0
+  if (keys.d.pressed) player.velocity.x = 5
+  else if (keys.a.pressed) player.velocity.x = -5 
 }
 
 animate()
@@ -56,14 +85,24 @@ animate()
 window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'd':
-      player.velocity.x = 1
+      keys.d.pressed = true
       break
     case 'a':
-      player.velocity.x = -1
+      keys.a.pressed = true
       break
     case 'w':
-      player.velocity.y = -15
-      break    
+      if (player.velocity.y === 0) player.velocity.y = -20
+      break
   }
+})
 
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+  }
 })
